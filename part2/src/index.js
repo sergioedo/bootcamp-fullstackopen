818525ad91c502f02f4cb9455f41f3c4b9409a42
Sergio Edo <sergio.edo@gmail.com>
2021-02-28 22:11:16 +0100

    part 2

---
+++ part2/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {
+        course.parts.map(part => <Part key={part.name} part={part} />)
+      }
+    </>
+  )
+}
+
+const Total = ({ course }) => {
+  const total = course.parts.reduce((accum, current) => accum + current.exercises, 0)
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
